f66fa5189865606854853230b2663f636c07f98d
Creativious <timothyu120@gmail.com>
2022-07-22 18:24:10 -0400

    SQL Update

--- creativiousUtilities/sql/__init__.py
+++ creativiousUtilities/sql/__init__.py
@@ -4,78 +4,35 @@ import psycopg2
 import psycopg2.extensions
 from creativiousUtilities.logging import Logger
 import mysql.connector
+from mysql.connector import errorcode
 import uuid
-
-# @DeprecationWarning
-# class PostsqeSQLController:
-#     def __init__(self, dbname: str = "postgres", host: str = "localhost", port: int = 5432, user: str = "postgres", password: str = "Temp1234!"):
-#         """Provides interface with PostgreSQL type databases with easy access without needing to use SQL language
-#         Currently only supports Asynchronous type connections"""
-#         self.dbname: str = dbname
-#         self.logger = Logger(name="SQLController", subname=dbname, debug=False).getLogger()
-#         self.logger.debug("SQLController Object Created")
-#         self.__connectionInfo = {
-#             "dbname": dbname,
-#             "host": "localhost",
-#             "port": port,
-#             "user": user,
-#             "password": password
-#
-#         }
-#         self.connection: psycopg2.extensions.connection = None
-#         self.cursor: psycopg2.extensions.cursor = None
-#         self.__connected:bool = False
-#         self.__connect()
-#     def __connect(self):
-#         self.logger.debug(f"Attempting connection to Database ({str(self.dbname)})")
-#         if not self.__connected:
-#             try:
-#                 self.connection = psycopg2.connect(
-#                     dbname = self.__connectionInfo["dbname"],
-#                     host = self.__connectionInfo["host"],
-#                     port = self.__connectionInfo["port"],
-#                     user = self.__connectionInfo["user"],
-#                     password = self.__connectionInfo["password"]
-#                 )
-#                 self.__connected = True
-#                 self.cursor = self.connection.cursor()
-#                 self.logger.debug(f"Established successful connection to Database ({str(self.dbname)})")
-#             except Exception as e:
-#                 logging.error(str(e))
-#         else:
-#             self.logger.warning(f"Attempted to connect to Database ({str(self.dbname)}) but already connected!")
-#
-#     def shutdown(self):
-#         if self.__connected:
-#             try:
-#                 self.cursor.close()
-#                 self.connection.close()
-#                 self.logger.debug("Turned off SQL Connection and Cursor!")
-#             except Exception as e:
-#                 logging.error(str(e))
-#
-#     def createTable(self, name):
-#         pass
-#
-#     class Table:
-#         def __init__(self, name):
-#             self.name = name
-
 
 
 class MySQL:
     def __init__(self):
-        self.connections = {
-
-        }
-        self.__connection_template = {"object": None}
+        self.connection : mysql.connector.MySQLConnection = None
         pass
     def connect(self, host: str, user: str, password: str, database: str, port: int = 3306):
-        ID = str(uuid.uuid4().int)
-        connection = {}
-        connection["object"] = mysql.connector.connect(host=host, user=user, password=password, database=database, port=port)
-        self.connections[ID] = connection
-        return self.connections[ID]
+        if self.connection is not None:
+            try:
+                ID = str(uuid.uuid4().int)
+                connection = {}
+                self.connection : mysql.connector.MySQLConnection = mysql.connector.connect(host=host, user=user, password=password, database=database, port=port)
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                    raise "Connection to database has failed due to wrong password or username!"
+                elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                    raise "Database doesn't exist!"
+                else:
+                    raise err
+            return self.connection
+        else:
+            return None
+    def disconnect(self):
+        self.connection.close()
+        self.connection = None
+
+
 
 
 
